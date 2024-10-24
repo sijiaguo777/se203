d3a4d74e962e69ffae418e2410a16f7ce58af8fa
Catherine-666 <guosijia2002@sjtu.edu.cn>
2024-10-24 15:10:15 +0200

    cmsis

--- TP/led.h
+++ TP/led.h
@@ -1,14 +1,5 @@
 #ifndef LED_H
 #define LED_H
-
-// #define RCC_AHB2ENR (*(volatile unsigned int *)0x4002104C) // Registre d'activation de l'horloge pour les ports GPIO
-// #define RCC_AHB2ENR_GPIOBEN (1 << 1)                       // Activation de l'horloge pour le port GPIOB
-// #define RCC_AHB2ENR_GPIOCEN (1 << 2)                      // Activation de l'horloge pour le port GPIOC
-// #define GPIOB_MODER (*(volatile unsigned int *)0x48000400) // Registre de mode pour le port GPIOB
-// #define GPIOC_MODER (*(volatile unsigned int *)0x48000800) // Registre de mode pour le port GPIOC
-// #define GPIOB_BSRR (*(volatile unsigned int *)0x48000418) // Registre de réinitialisation et de réglage pour le port GPIOB
-// #define GPIOC_BSRR (*(volatile unsigned int *)0x48000818) // Registre de réinitialisation et de réglage pour le port GPIOC
-
 #endif // LED_H
 #include "stm32l475xx.h"
 void led_init(void);  // initialiser LED
