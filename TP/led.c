1f4a2c62f939be2d6d1f3943de4fcbe06b21cead
Catherine-666 <guosijia2002@sjtu.edu.cn>
2024-10-24 11:40:08 +0200

    TP1024

---
+++ TP/led.c
@@ -0,0 +1,20 @@
+#include "led.h"
+
+int main(void)
+{
+    led_init();
+    // for (int i = 0; i < 1000; i++)
+    // {
+    //     led(LED_GREEN);
+    //     asm volatile("nop");
+    //     led(LED_GREEN_OFF);
+    // }
+    // led(LED_GREEN_OFF);
+    led(LED_BLUE);
+    led(LED_YELLOW);
+    led(LED_GREEN);
+
+    led(LED_BLUE_YELLOW_OFF);
+    led(LED_GREEN_OFF);
+    return 0;
+}
