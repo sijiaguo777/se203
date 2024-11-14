a0c48bb62205d2e0d568f5ec8a7d25238db56f2d
Catherine-666 <guosijia2002@sjtu.edu.cn>
2024-11-14 17:11:09 +0100

    irq

--- TP/main.c
+++ TP/main.c
@@ -1,24 +1,40 @@
 #include "stm32l475xx.h"
 #include "clocks.h"
 #include "matrix.h"
-#include <stddef.h>
-#include <stdint.h>
+#include "buttons.h"
 
-// UART Ports:
-// ===================================================
-// PB.6 = USART1_TX (AF7)  |  PB.7 = USART1_RX (AF7)
+// int infini(){
+//     int i = 1;
+//     while (1)
+//     {
+//     }
+//     return 0;
+// }
+
+// int fibo(int n){
+//     if (n == 0) {
+//         return 0;
+//     } else if (n == 1) {
+//         return 1;
+//     } else {
+//         return fibo(n - 1) + fibo(n - 2);
+//     }
+// }
 
 extern uint8_t* _binary_image_raw_start;
 extern uint8_t* _binary_image_raw_end;
 extern int _binary_image_raw_size;
 
-
-int main()
-{
+int main(void) {
+    clocks_init();
+    // irq_init();
+    // led_init();
     matrix_init();
-    // test_pixels();
-    
-    while(1){
+    // button_init();
+    // uart_init(); 
+
+    while (1)
+    {
         display_image_static((uint8_t*)&_binary_image_raw_start, (uint8_t*)&_binary_image_raw_end, (int)_binary_image_raw_size);
     }
    
