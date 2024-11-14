6293d983fcbe4cc1a3b623a383a5fee20135a2e1
Catherine-666 <guosijia2002@sjtu.edu.cn>
2024-11-14 15:46:36 +0100

    Merge tag 'TEST_MATRIX' into final-workflow

--- TP/main.c
+++ TP/main.c
@@ -1,13 +1,7 @@
 #define STM32L475xx
 #include "stm32l475xx.h"
-<<<<<<< HEAD
-#include "stm32l4xx.h"
-#include "clocks.h"
-#include "uart.h"
-=======
 #include "clocks.h"
 #include "matrix.h"
->>>>>>> TEST_MATRIX
 #include <stddef.h>
 #include <stdint.h>
 
@@ -15,45 +9,12 @@
 // ===================================================
 // PB.6 = USART1_TX (AF7)  |  PB.7 = USART1_RX (AF7)
 
-<<<<<<< HEAD
-int main()
-{
-    clocks_init();
-    uart_init();
-
-    // uart_puts("Hello World!");
-
-    // programme d'écho
-    // uart_puts("Tapez quelque chose:");
-    // char buffer[100];
-    // while (1)
-    // {
-    //     uart_gets(buffer, sizeof(buffer) - 1);
-    //     uart_puts("Vous avez tapé: ");
-    //     uart_puts(buffer);
-    // }
-
-    // programme de checksum
-    uart_puts("Checksum 32: \n");
-    while (1)
-    {
-        uint32_t checksum = calculate_checksum(1000);
-        uart_putchar((checksum >> 24) & 0xFF);
-        uart_putchar((checksum >> 16) & 0xFF);
-        uart_putchar((checksum >> 8) & 0xFF);
-        uart_putchar(checksum & 0xFF);
-        
-        uart_putchar('\r');
-        uart_putchar('\n');
-    }
-=======
 
 
 int main()
 {
     matrix_init();
     test_pixels();
->>>>>>> TEST_MATRIX
 
     return 0;
 }
