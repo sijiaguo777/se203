29533a008bb3c3e526d8f8ef277e651162460641
Catherine-666 <guosijia2002@sjtu.edu.cn>
2024-11-15 00:42:00 +0100

    solve random matrix colors

--- TP/uart.d
+++
@@ -1,7 +0,0 @@
-uart.o: uart.c CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h \
- CMSIS/Include/core_cm4.h CMSIS/Include/core_cmInstr.h \
- CMSIS/Include/cmsis_gcc.h CMSIS/Include/core_cmFunc.h \
- CMSIS/Include/core_cmSimd.h \
- CMSIS/Device/ST/STM32L4xx/Include/system_stm32l4xx.h \
- CMSIS/Device/ST/STM32L4xx/Include/stm32l4xx.h \
- CMSIS/Device/ST/STM32L4xx/Include/stm32l475xx.h clocks.h
